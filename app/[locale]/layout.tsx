1414284a4879988a3b4c22a40af1c1294427633f
InnaSodri <turorainna@gmail.com>
2025-08-19 14:12:50 +0300

    fix: fix  root layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -11,9 +11,9 @@ export default async function LocaleLayout({
   params
 }: {
   children: ReactNode;
-  params: {locale: string};
+  params: Promise<{locale: string}>;
 }) {
-  const {locale} = params;
+  const {locale} = await params;
   if (!isLocale(locale)) notFound();
   const messages = await getMessages({locale});
 
